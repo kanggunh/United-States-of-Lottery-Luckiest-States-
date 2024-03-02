97e1edaf3da9277b4fa8241a43b50aea4e041eed
Kang Gun Ham <kham@ucsd.edu>
2024-03-02 14:47:47 -0800

    first push kanggun

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,1 +1,183 @@
 
+<script>
+    import Graph from '../components/Graph.svelte';
+    import Bar from '../components/Bar.svelte';
+
+    let show = 0;
+    let toggle_1 = false;
+    let toggle_2 = false;
+    let ans;
+
+    function update1(text) {
+        toggle_1 = true;
+        ans = text;
+    }
+
+    function update2(text) {
+        toggle_2 = true;
+        ans = text;
+    }
+
+    const select_1 = () => update1('Not really...',);
+    const select_2 = () => update1('Yes, almost correct!');
+    const select_3 = () => update1('Pretty much!');
+
+</script>
+
+
+<main>
+    <h1>Scatterplot</h1>
+
+    <h2> Which income class do you think participates in lottery the most? </h2>
+    <div class="btn_group">
+        <button on:click={select_1}> (a) Lower-income </button>
+        <button on:click={select_2}> (b) Middle-income </button>
+        <button on:click={select_3}> (c) Upper-income </button>
+    </div>   
+    <br> 
+    {#if toggle_1}
+        <div class="hide">
+            <h3>{ans}</h3>
+            <p>The Powerball lottery has a very specific demographic regarding income earners.
+                <li>56% of people who make $36,000 and $90,000 annually buy at least one lottery ticket.</li>
+                <li>53% of people who make more than $90,000 annually do the same.</li>
+                <br>
+                And only 40% percent of people how earn less than $36,000 per year buy lottery tickets.
+            </p>
+        </div>
+        <h3>Lottery Sales based on Median Income by states</h3>
+        <p style="text-align: center; font-size: 12px;">In Ten Millions of Dollars vs. Median Income</p>
+        <Graph />
+        <br>
+    {/if}
+
+    {#if toggle_2}
+        <div class="hide">
+            <h3>{ans}</h3>
+            <p>The Powerball lottery has a very specific demographic regarding income earners.
+                <li>56% of people who make $36,000 and $90,000 annually buy at least one lottery ticket.</li>
+                <li>53% of people who make more than $90,000 annually do the same.</li>
+                <br>
+                And only 40% percent of people how earn less than $36,000 per year buy lottery tickets.
+            </p>
+        </div>
+        <Bar />
+        <br>
+    {/if}
+    <br>
+
+    <p> Write up: <br>
+        So far we have implemented pie chart that shows some 'fun fact' about where lottery winners spend their money. 
+        Then we have a button that let's user guess their hypothesis before showing the data. Doing so makes the user think
+        before mindlessly viewing the visualization so that they remember they retain this information. 
+        At the moment, we are planning to use 'scrollytelling' to first show the above pie charts then let the 
+        user explore other data at the end with scatterplots and barcharts about the powerball. The most challenging part will 
+        be to implement the scrolling and animating the pie chart to move along with the scroll.
+
+    <br>
+    <br>
+    <a href="https://www.flaticon.com/free-icons/bingo" title="bingo icons" style="font-size: 10px;">Bingo icons created by Freepik - Flaticon</a>
+</main>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Protest+Strike&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100..900;1,8..144,100..900&display=swap');
+
+    :root {
+        --color-bg: #ffffff;
+        --color-outline: #c2c2c2;
+        --color-shadow: hsl(0, 0%, 0%, 0.1);
+        --color-text: #3f4252;
+        --color-bg-1: hsla(0, 0%, 0%, 0.2);
+        --color-shadow-1: hsl(0, 0%, 96%);
+        background-color: #FCFCFC;
+    }
+
+    main {
+        text-align: center;
+        font-family: "Roboto Serif", serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15px;
+        color: var(--color-text);
+        width: 1000px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 50px;
+        background-color: #F3F3F3;
+    }
+
+    h1 {
+        margin-top: -25px;
+        font-size: 5em;
+        font-weight: 600;
+        line-height: 2;
+    }
+
+    h2 {
+        width: 800px;
+        font-size: 16px;
+        margin-left: auto;
+        margin-right: auto; 
+        text-align: left;  
+        /* background-color: powderblue; */
+    }
+
+    p {
+        width: 800px;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: left;
+        line-height: 2.2;
+        font-size: px;
+    }
+
+    button {
+        background-color: #FCFCFC;
+        font-family: "Roboto Serif", serif;
+        font-size: 13px;
+        color: black;
+        border: 1.5px solid #1B1717; 
+        transition-duration: 0.2s;
+        text-align: center;
+        border-radius: 5px;
+        padding: 15px 28px;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        font-weight: 500;
+    }
+
+    button:hover {
+        background-color: #d3d3d3; 
+        color: white;
+    }
+
+    button:active {
+        background: #2e2e2e;
+        border: 1.5px solid black;
+        color: white;
+    }
+
+    h2 {
+        width: 800px;
+        margin-left: auto;
+        margin-right: auto; 
+        text-align: left;
+    }
+
+    div.btn_group {
+        width: 800px;
+        text-align: left;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    div.hide {
+        width: 800px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 50px;
+        text-align: left;
+        /* background-color: powderblue; */
+    }
+</style>
--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -1,3 +1,108 @@
 <script>
-    
+    import * as d3 from 'd3';
+    import { onMount } from 'svelte'; 
+    import Tooltip from '../components/Tooltip.svelte';
+
+    let width= 460;
+    let height= 400;
+    const margin = { top: 20, right: 20, bottom: 50, left: 90 };
+    const innerHeight = height - margin.top - margin.bottom;
+    const innerWidth = width - margin.left - margin.right;
+
+    let data_lottery = []
+    let xTicks = []
+    let yTicks = []
+
+    onMount(async () => {
+        const res = await fetch(
+            'lottery_stats.csv',
+        );
+        const csv = await res.text();
+        await d3.csvParse(csv, data => {
+        data_lottery.push(data);
+        });
+        data_lottery = data_lottery;
+    });
+
+    let xAxis;
+	let yAxis;
+
+    console.log(data_lottery);
+
+    import { max } from "d3-array";
+    import { min} from "d3-array";
+
+    $: xScale = d3.scaleLinear()
+        .domain([min(data_lottery, d => d.income_two), max(data_lottery, d => d.income_two)])
+        .range([margin.left, width - margin.right]);
+
+
+    $: yScale = d3.scaleLinear()
+        .domain([min(data_lottery, d => d.sales_in_million), max(data_lottery, d => d.sales_in_million)])
+        .range([height - margin.bottom, margin.top]);
+
+    $: {
+        d3.select(yAxis)
+            .call(d3.axisLeft(yScale));
+        
+        d3.select(xAxis)
+				.call(d3.axisBottom(xScale))
+				.selectAll('.tick > text')
+				.attr('y', 0)
+				.attr('dy', '0.35em')
+				.attr('dx', '-1em')
+				.attr('text-anchor', 'end')
+				.attr('transform', 'rotate(-90)');
+	}
+
+    let hoveredData;
+    $: console.log(hoveredData);
+    $: console.log(window.innerHeight);
+    $: console.log(window.innerWidth);
+
 </script>
+
+{#if hoveredData}
+    <Tooltip data={hoveredData} {xScale} {yScale} />
+{/if}
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<svg {width} {height} 
+    on:mouseleave={() => {
+        setTimeout(() => {
+            hoveredData = null;
+        }, 100);
+    }}
+>
+   
+    {#each data_lottery as point}
+        <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+        <circle 
+        cx={xScale(point.income_two)} 
+        cy={yScale(point.sales_in_million)} 
+        r={hoveredData && hoveredData == point ? "12.5": "8"}
+        opacity={hoveredData ? hoveredData == point ? "0.8": "0.45" : "0.8"}
+        on:mouseover={() => {hoveredData = point;}}
+         />
+    {/each}
+
+    <g transform="translate(0, {height - margin.bottom})"
+    bind:this={xAxis} />
+
+    <g transform="translate({margin.left}, 0)"
+    bind:this={yAxis} />
+</svg>
+
+
+<style>
+    circle {
+        stroke: gray;
+        fill: lightblue;
+        transition: all 300ms ease;
+    }
+
+    svg {
+        align-items: center;
+        margin-right: 60px;
+    }
+</style>
